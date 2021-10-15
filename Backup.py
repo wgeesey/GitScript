9df0e0f907eee03a4f19307a7a5b140df67a7d13
wgeesey <89809942+wgeesey@users.noreply.github.com>
2021-10-15 10:15:24 -0400

    Update Backup.py
    Change import datetime, date >> import date

--- Backup.py
+++ Backup.py
@@ -3,7 +3,7 @@
 import os
 from netmiko import ConnectHandler
 from getpass import getpass
-from datetime import datetime, date
+from datetime import date
 
 USERNAME = input("Please enter your username: ")
 PASSWORD = getpass("Please enter your password: ")
